11666c1ce19aa6c77d3335b3f2f022e3b811d2c8
Pedro Rodriguez <prodriguez@trulia.com>
2015-02-05 09:33:58 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Placeholder README file
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ScalaFunctional',
+    description="Scala functional programming style in Python",
+    url="https://github.com/EntilZha/ScalaFunctional",
+    author="Pedro Rodriguez",
+    author_email="pedro@snowgeek.org",
+    license="MIT",
+    keywords="functional scala",
+    package_data=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    version="0.0.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: 2.7"
+    ]
+)
