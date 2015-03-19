3a986a469c110aa0f7c7611c190bda4ba1cb9011
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-03-19 00:14:57 -0700

    added significant ammounts of documentation

--- functional/chain.py
+++ functional/chain.py
@@ -5,6 +5,22 @@ from functools import reduce
 
 class FunctionalSequence(object):
     def __init__(self, sequence):
+        """
+        Takes a sequence and wraps it around a FunctionalSequence object.
+
+        If the sequence
+        is already an instance of FunctionalSequence, __init__ will insure that it is
+        at most wrapped exactly once.
+
+        If the sequence is a list or tuple, it is set as the sequence.
+
+        If it is an iterable, then it is expanded into a list then set to the sequence
+
+        If the object does not fit any of these classes, a TypeError is thrown
+
+        :param sequence: sequence of items to wrap in a FunctionalSequence
+        :return: sequence wrapped in a FunctionalSequence
+        """
         if isinstance(sequence, FunctionalSequence):
             self.sequence = sequence._get_base_sequence()
         elif isinstance(sequence, list) or isinstance(sequence, tuple):
@@ -15,18 +31,40 @@ class FunctionalSequence(object):
             raise TypeError("Given sequence must be a list")
 
     def _get_base_sequence(self):
+        """
+        Retrieves the root sequence wrapped by one or more FunctionalSequence objects
+
+        :return: root sequence
+        """
         if isinstance(self.sequence, FunctionalSequence):
             return self.sequence._get_base_sequence()
         else:
             return self.sequence
 
     def __eq__(self, other):
+        """
+        Checks for equality with the sequence's equality operator
+
+        :param other: object to compare to
+        :return: true if the underlying sequence is equal to other
+        """
         return self.sequence == other
 
     def __ne__(self, other):
+        """
+        Checks for inequality with the sequence's inequality operator
+
+        :param other: object to compare to
+        :return: true if the underlying sequence is not equal to other
+        """
         return self.sequence != other
 
     def __hash__(self):
+        """
+        Return the hash of the sequence
+
+        :return: hash of sequence
+        """
         return hash(self.sequence)
 
     def __repr__(self):
@@ -78,6 +116,8 @@ class FunctionalSequence(object):
         Traceback (most recent call last):
          ...
         IndexError: list index out of range
+
+        :return: first element of sequence
         """
         return _wrap(self.sequence[0])
 
@@ -94,6 +134,8 @@ class FunctionalSequence(object):
         Traceback (most recent call last):
          ...
         IndexError: list index out of range
+
+        :return: first element of sequence
         """
         return _wrap(self.sequence[0])
 
@@ -106,6 +148,8 @@ class FunctionalSequence(object):
 
         >>> seq([]).head_option()
         None
+
+        :return: first element of sequence or None if sequence is empty
         """
         if not self.sequence:
             return None
@@ -124,6 +168,8 @@ class FunctionalSequence(object):
         Traceback (most recent call last):
          ...
         IndexError: list index out of range
+
+        :return: last element of sequence
         """
         return _wrap(self.sequence[-1])
 
@@ -136,6 +182,8 @@ class FunctionalSequence(object):
 
         >>> seq([]).last_option()
         None
+
+        :return: last element of sequence or None if sequence is empty
         """
         if not self.sequence:
             return None
@@ -147,6 +195,8 @@ class FunctionalSequence(object):
 
         >>> seq([1, 2, 3]).init()
         [1, 2]
+
+        :return: sequence without last element
         """
         return FunctionalSequence(self.sequence[:-1])
 
@@ -156,6 +206,8 @@ class FunctionalSequence(object):
 
         >>> seq([1, 2, 3]).init()
         [2, 3]
+
+        :return: sequence without first element
         """
         return FunctionalSequence(self.sequence[1:])
 
@@ -165,8 +217,10 @@ class FunctionalSequence(object):
 
         >>> seq([1, 2, 3]).inits()
         [[1, 2, 3], [1, 2], [1], []]
+
+        :return: consecutive init()s on sequence
         """
-        result = [_wrap(self.sequence[:i]) for i in reversed(range(len(self.sequence)+1))]
+        result = [_wrap(self.sequence[:i]) for i in reversed(range(len(self.sequence) + 1))]
         return FunctionalSequence(result)
 
     def tails(self):
@@ -175,47 +229,129 @@ class FunctionalSequence(object):
 
         >>> seq([1, 2, 3]).tails()
         [[1, 2, 3], [2, 3], [3], []]
+
+        :return: consecutive tail()s of the sequence
         """
-        result = [_wrap(self.sequence[i:]) for i in range(len(self.sequence)+1)]
+        result = [_wrap(self.sequence[i:]) for i in range(len(self.sequence) + 1)]
         return FunctionalSequence(result)
 
     def drop(self, n):
+        """
+        Drop the first n elements of the sequence.
+
+        >>> seq([1, 2, 3, 4, 5]).drop(2)
+        [3, 4, 5]
+
+        :param n: number of elements to drop
+        :return: sequence without first n elements
+        """
         return FunctionalSequence(self.sequence[n:])
 
     def drop_while(self, f):
+        """
+        Drops elements in the sequence while f evaluates to True, then returns the rest.
+
+        >>> seq([1, 2, 3, 4, 5, 1, 2]).drop_while(lambda x: x < 3)
+        [3, 4, 5, 1, 2]
+
+        :param f: truth returning function
+        :return: elements including and after f evaluates to False
+        """
+        i = 0
         for i, e in enumerate(self.sequence):
             if not f(e):
                 break
         return self.drop(i)
 
     def take(self, n):
+        """
+        Take the first n elements of the sequence.
+
+        >>> seq([1, 2, 3, 4]).take(2)
+        [1, 2]
+
+        :param n: number of elements to take
+        :return: first n elements of sequence
+        """
         return FunctionalSequence(self.sequence[:n])
 
     def take_while(self, f):
+        """
+        Take elements in the sequence until f evaluates to False, then return them.
+
+        >>> seq([1, 2, 3, 4, 5, 1, 2]).take_while(lambda x: x < 3)
+        [1, 2]
+
+        :param f: truth returning function
+        :return: elements taken until f evaluates to False
+        """
+        i = 0
         for i, e in enumerate(self.sequence):
             if not f(e):
                 break
         return self.take(i)
 
     def map(self, f):
+        """
+        Maps f onto the elements of the sequence.
+
+        >>> seq([1, 2, 3, 4]).map(lambda x: x * -1)
+        [-1, -2, -3, -4]
+
+        :param f: function to map with
+        :return: sequence with f mapped onto it
+        """
         return FunctionalSequence(map(f, self.sequence))
 
     def for_each(self, f):
+        """
+        Executes f on each element of the sequence.
+
+        >>> l = []
+        >>> seq([1, 2, 3, 4]).for_each(l.append)
+        >>> l
+        [1, 2, 3, 4]
+
+        :param f: function to execute
+        :return: None
+        """
         for e in self.sequence:
             f(e)
 
     def filter(self, f):
+        """
+        Filters sequence to include only elements where f is True.
+
+        >>> seq([-1, 1, -2, 2]).filter(lambda x: x > 0)
+        [1, 2]
+
+        :param f: function to filter on
+        :return: filtered sequence
+        """
         return FunctionalSequence(filter(f, self.sequence))
 
     def filter_not(self, f):
+        """
+        Filters sequence to include only elements where f is False.
+
+        >>> seq([-1, 1, -2, 2]).filter_not(lambda x: x > 0)
+        [-1, -2]
+
+        :param f: function to filter_not on
+        :return: filtered sequence
+        """
         g = lambda x: not f(x)
         return FunctionalSequence(filter(g, self.sequence))
 
     def count(self, f):
         """
-        Counts the number of elements in the sequence which satisfy the predicate f
+        Counts the number of elements in the sequence which satisfy the predicate f.
+
+        >>> seq([-1, -2, 1, 2]).count(lambda x: x > 0)
+        2
+
         :param f: predicate to count elements on
-        :return: count satisfying predicate
+        :return: count of elements that satisf predicate
         """
         n = 0
         for e in self.sequence:
@@ -224,30 +360,111 @@ class FunctionalSequence(object):
         return n
 
     def len(self):
+        """
+        Return length of sequence using its length function.
+
+        >>> seq([1, 2, 3]).len()
+        3
+
+        :return: length of sequence
+        """
         return len(self.sequence)
 
     def size(self):
-        return len(self.sequence)
+        """
+        Return size of sequence using its length function.
+
+        :return: size of sequence
+        """
+        return self.len()
 
     def empty(self):
+        """
+        Returns True if the sequence has length zero.
+
+        >>> seq([]).empty()
+        True
+
+        >>> seq([1]).empty()
+        False
+
+        :return: True if sequence length is zero
+        """
         return len(self.sequence) == 0
 
     def non_empty(self):
+        """
+        Returns True if the sequence does not have length zero.
+
+        >>> seq([]).non_empty()
+        False
+
+        >>> seq([1]).non_empty()
+        True
+
+        :return: True if sequence length is not zero
+        """
         return len(self.sequence) != 0
 
     def any(self):
+        """
+        Returns True if any element in the sequence has truth value True
+
+        >>> seq([True, False]).any()
+        True
+
+        >>> seq([False, False]).any()
+        False
+
+        :return: True if any element is True
+        """
         return any(self.sequence)
 
     def all(self):
+        """
+        Returns True if the truth value of all items in the sequence true.
+
+        >>> seq([True, True]).all()
+        True
+
+        >>> seq([True, False]).all()
+        False
+
+        :return: True if all items truth value evaluates to True
+        """
         return all(self.sequence)
 
     def exists(self, f):
+        """
+        Returns True if an element in the sequence makes f evaluate to True.
+
+        >>> seq([1, 2, 3, 4]).exists(lambda x: x == 2)
+        True
+
+        >>> seq([1, 2, 3, 4]).exists(lambda x: x < 0)
+        False
+
+        :param f: existence check function
+        :return: True if any element satisfies f
+        """
         for e in self.sequence:
             if f(e):
                 return True
         return False
 
     def for_all(self, f):
+        """
+        Returns True if all elements in sequence make f evaluate to True.
+
+        >>> seq([1, 2, 3]).for_all(lambda x: x > 0)
+        True
+
+        >>> seq([1, 2, -1]).for_all(lambda x: x > 0)
+        False
+
+        :param f: function to check truth value of all elements with
+        :return: True if all elements make f evaluate to True
+        """
         for e in self.sequence:
             if not f(e):
                 return False
@@ -356,6 +573,16 @@ class FunctionalSequence(object):
         return _wrap(min(self.sequence, key=f))
 
     def find(self, f):
+        """
+        Finds the first element of the sequence that satisfies f. If no such element exists,
+        then return None.
+
+        >>> seq(["abc", "ab", "bc"]).find(lambda x: len(x) == 2)
+        'ab'
+
+        :param f: function to find with
+        :return: first element to satisfy f or None
+        """
         for e in self.sequence:
             if f(e):
                 return e
@@ -363,15 +590,49 @@ class FunctionalSequence(object):
             return None
 
     def flatten(self):
+        """
+        Flattens a sequence of sequences to a single sequence of elements.
+
+        >>> seq([[1, 2], [3, 4], [5, 6]])
+        [1, 2, 3, 4, 5, 6]
+
+        :return: flattened sequence
+        """
         return self.flat_map(lambda x: x)
 
     def flat_map(self, f):
+        """
+        Applies f to each element of the sequence, which themselves should be sequences.
+        Then appends each element of each sequence to a final result
+
+        >>> seq([[1, 2], [3, 4], [5, 6]]).flat_map(lambda x: x)
+        [1, 2, 3, 4, 5, 6]
+
+        >>> seq(["a", "bc", "def"]).flat_map(list)
+        ['a', 'b', 'c', 'd', 'e', 'f']
+
+        >>> seq([[1], [2], [3]]).flat_map(lambda x: x * 2)
+        [1, 1, 2, 2, 3, 3]
+
+        :param f: function to apply to each sequence in the sequence
+        :return: application of f to elements followed by flattening
+        """
         l = []
         for e in self.sequence:
             l.extend(f(e))
         return FunctionalSequence(l)
 
     def group_by(self, f):
+        """
+        Group elements into a list of (Key, Value) tuples where f creates the key and maps
+        to values matching that key.
+
+        >>> seq(["abc", "ab", "z", "f", "qw"]).group_by(len)
+        [(1, ['z', 'f']), (2, ['ab', 'qw']), (3, ['abc'])]
+
+        :param f: group by result of this function
+        :return: grouped sequence
+        """
         result = {}
         for e in self.sequence:
             if result.get(f(e)):
@@ -381,6 +642,14 @@ class FunctionalSequence(object):
         return FunctionalSequence(result.items())
 
     def group_by_key(self):
+        """
+        Group sequence of (Key, Value) elements by Key.
+
+        >>> seq([('a', 1), ('b', 2), ('b', 3), ('b', 4), ('c', 3), ('c', 0)]).group_by_key()
+        [('a', [1]), ('c', [3, 0]), ('b', [2, 3, 4])]
+
+        :return: sequence grouped by key
+        """
         result = {}
         for e in self.sequence:
             if result.get(e[0]):
@@ -390,18 +659,61 @@ class FunctionalSequence(object):
         return FunctionalSequence(result.items())
 
     def reduce_by_key(self, f):
+        """
+        Reduces a sequence of (Key, Value) using f on each Key.
+
+        >>> seq([('a', 1), ('b', 2), ('b', 3), ('b', 4), ('c', 3), ('c', 0)]).reduce_by_key(lambda x, y: x + y)
+        [('a', 1), ('c', 3), ('b', 9)]
+
+        :param f: reduce each list of values using two parameter, associative f
+        :return: Sequence of tuples where the value is reduced with f
+        """
         return self.group_by_key().map(lambda kv: (kv[0], reduce(f, kv[1])))
 
     def reduce(self, f):
+        """
+        Reduce sequence of elements using f.
+
+        >>> seq([1, 2, 3]).reduce(lambda x, y: x + y)
+        6
+
+        :param f: two parameter, associative reduce function
+        :return: reduced value using f
+        """
         return reduce(f, self.sequence)
 
     def make_string(self, separator):
+        """
+        Concatenate the elements of the sequence into a string separated by separator.
+
+        >>> seq([1, 2, 3]).make_string("@")
+        '1@2@3'
+
+        :param separator: string separating elements in string
+        :return: concatenated string separated by separator
+        """
         return separator.join(str(e) for e in self.sequence)
 
     def product(self):
+        """
+        Takes product of elements in sequence.
+
+        >>> seq([1, 2, 3, 4]).product()
+        24
+
+        :return: product of elements in sequence
+        """
         return self.reduce(lambda x, y: x * y)
 
     def sum(self):
+        """
+        Takes sum of elements in sequence.
+
+        >>> seq([1, 2, 3, 4]).sum()
+        10
+
+        :return: sum of elements in sequence
+        """
         return sum(self.sequence)
 
     def fold_left(self, zero_value, f):
@@ -410,7 +722,12 @@ class FunctionalSequence(object):
         parameters, 1) the value to accumulate into the result and 2) the current result.
         Elements are folded left to right.
 
+        >>> seq(['a', 'bc', 'de', 'f', 'm', 'nop']).fold_left("Start:", lambda v, curr: curr + 2 * v)
+        'Start:aabcbcdedeffmmnopnop'
+
         :param zero_value: zero value to reduce into
+        :param f: Two parameter function. First parameter is value to be accumulated into the result. Second parameter
+        is the current result
         :return: value from folding values with f into zero_value
         """
         result = zero_value
@@ -424,7 +741,12 @@ class FunctionalSequence(object):
         parameters, 1) the value to accumulate into the result and 2) the current result.
         Elements are folded right to left.
 
+        >>> seq(['a', 'bc', 'de', 'f', 'm', 'nop']).fold_right("Start:", lambda v, curr: curr + 2 * v)
+        'Start:nopnopmmffdedebcbcaa'
+
         :param zero_value: zero value to reduce into
+        :param f: Two parameter function. First parameter is value to be accumulated into the result. Second parameter
+        is the current result
         :return: value from folding values with f into zero_value
         """
         return self.reverse().fold_left(zero_value, f)
