9b3588ec30b8ac91a7bd57169b8b7e198885d25c
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-09-18 14:07:23 -0600

    fix travis for python 2.7, 3.0, 3.1, 3.2, and 3.3

--- .travis-install.sh
+++ .travis-install.sh
@@ -2,7 +2,7 @@
 python -V 2> pyversion
 version=$(cat pyversion)
 rm pyversion
-match=$(echo "$version" | grep "2\.7")
+match=$(echo "$version" | grep -E "(2\.7)|(3\.0)|(3\.1)|(3\.2)|(3\.3)")
 if [ "$match" != "" ]
 then
     pip install enum34
