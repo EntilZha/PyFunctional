123f263cae9c7aa89d6e977d7848e1a30a787081
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-09-18 23:07:30 -0600

    Added documentation for open

--- functional/streams.py
+++ functional/streams.py
@@ -8,8 +8,10 @@ from .util import is_primitive
 
 def seq(*args):
     """
-    Alias function for creating a new Sequence. Additionally it also parses various types
-    of input to a Sequence as best it can.
+    Primary entrypoint for the functional package. Returns a functional.pipeline.Sequence wrapping
+    the original sequence.
+
+    Additionally it parses various types of input to a Sequence as best it can.
 
     >>> type(seq([1, 2]))
     functional.pipeline.Sequence
@@ -48,8 +50,24 @@ def seq(*args):
 
 def open(path, delimiter=None, mode='r', buffering=-1, encoding=None,
          errors=None, newline=None):
+    """
+    Additional entry point to Sequence which parses input files as defined by options. Path
+    specifies what file to parse. If delimiter is not None, then the file is read in bulk then
+    split on it. If it is None (the default), then the file is parsed as sequence of lines. The
+    rest of the options are passed directly to builtins.open with the exception that write/append
+    file modes is not allowed.
+
+    :param path: path to file
+    :param delimiter: delimiter to split joined text on. if None, defaults to file.readlines()
+    :param mode: file open mode
+    :param buffering: passed to builtins.open
+    :param encoding: passed to builtins.open
+    :param errors: passed to builtins.open
+    :param newline: passed to builtins.open
+    :return: output of file depending on options wrapped in a Sequence via seq
+    """
     if not re.match('^[rbt]{1,3}$', mode):
-        raise ValueError("mode argument must be only have r, b, and t")
+        raise ValueError('mode argument must be only have r, b, and t')
     with builtins.open(path, mode=mode, buffering=buffering, encoding=encoding, errors=errors,
                        newline=newline) as data:
         if delimiter is None:
