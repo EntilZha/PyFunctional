0f1e02a6796da83a3b3bf4185bf067d3fa31113d
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-09-21 17:34:42 -0600

    fix style error

--- functional/test/test_util.py
+++ functional/test/test_util.py
@@ -4,11 +4,11 @@ from ..util import LazyFile
 
 class TestUtil(unittest.TestCase):
     def test_lazy_file(self):
-        file = LazyFile('LICENSE.txt')
-        self.assertTrue(file.file is None)
-        iter(file)
-        handle_0 = file.file
-        iter(file)
-        handle_1 = file.file
+        license_file = LazyFile('LICENSE.txt')
+        self.assertTrue(license_file.file is None)
+        iter(license_file)
+        handle_0 = license_file.file
+        iter(license_file)
+        handle_1 = license_file.file
         self.assertTrue(handle_0.closed)
         self.assertFalse(handle_1.closed)
