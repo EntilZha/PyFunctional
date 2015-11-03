6aee5821ebff02cfab783db558d8ff3ebb2d02fe
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-11-03 16:00:27 -0700

    Cleaned up ReusableFile and documented util.py

--- functional/test/test_util.py
+++ functional/test/test_util.py
@@ -7,7 +7,6 @@ class TestUtil(unittest.TestCase):
         license_file_lf = ReusableFile('LICENSE.txt')
         with open('LICENSE.txt') as license_file:
             self.assertEqual(list(license_file), list(license_file_lf))
-        self.assertTrue(license_file_lf.file is None)
         iter_1 = iter(license_file_lf)
         iter_2 = iter(license_file_lf)
         self.assertEqual(list(iter_1), list(iter_2))
--- functional/util.py
+++ functional/util.py
@@ -46,19 +46,59 @@ def is_primitive(val):
 
 
 def identity(arg):
+    """
+    Function which returns the argument. Used as a default lambda function.
+
+    >>> obj = object()
+    >>> obj is identity(obj)
+    True
+
+    :param arg: object to take identity of
+    :return: return arg
+    """
     return arg
 
 
 def is_iterable(val):
+    """
+    Check if val is not a list, but is a collections.Iterable type. This is used to determine
+    when list() should be called on val
+
+    >>> l = [1, 2]
+    >>> is_iterable(l)
+    False
+    >>> is_iterable(iter(l))
+    True
+
+    :param val: value to check
+    :return: True if it is not a list, but is a collections.Iterable
+    """
     if isinstance(val, list):
         return False
     return isinstance(val, collections.Iterable)
 
 
 class ReusableFile(object):
+    """
+    Class which emulates the builtin file except that calling iter() on it will return separate
+    iterators on different file handlers (which are automatically closed when iteration stops). This
+    is useful for allowing a file object to be iterated over multiple times while keep evaluation
+    lazy.
+    """
     # pylint: disable=too-few-public-methods,too-many-instance-attributes
     def __init__(self, path, delimiter=None, mode='r', buffering=-1, encoding=None,
                  errors=None, newline=None):
+        """
+        Constructor arguments are passed directly to builtins.open
+        :param path: passed to open
+        :param delimiter: passed to open
+        :param mode: passed to open
+        :param buffering: passed to open
+        :param encoding: passed to open
+        :param errors: passed to open
+        :param newline: passed to open
+        :return: ReusableFile from the arguments
+        """
         # pylint: disable=too-many-arguments
         self.path = path
         self.delimiter = delimiter
@@ -67,9 +107,13 @@ class ReusableFile(object):
         self.encoding = encoding
         self.errors = errors
         self.newline = newline
-        self.file = None
 
     def __iter__(self):
+        """
+        Returns a new iterator over the file using the arguments from the constructor. Each call
+        to __iter__ returns a new iterator independent of all others
+        :return: iterator over file
+        """
         with builtins.open(self.path,
                            mode=self.mode,
                            buffering=self.buffering,
