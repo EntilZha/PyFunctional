dd614f7432cd8b967abc5871a18ba0923e315ce9
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-02-01 10:52:17 -0700

    Refactor build matrix to account for python 3.6 and pypy separately

--- travis.sh
+++ travis.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ $LINT ]]; then
-    nosetests --with-coverage --cover-package=functional && pylint functional
+if [[ $PY_36 -eq 1 ]]; then
+    pip install git+https://github.com/PyCQA/pylint.git@7daed7b8982480c868b0f642a5251f00ffb253c6
+    pip install git+https://github.com/PyCQA/astroid.git@d0b5acdfebcdda5c949584c32a8cbc0f31d5cf25
+else
+    pip install pylint
 fi
+
+if ! [[ $PYPY -eq 1 ]]; then
+    pip install pandas
+fi
+
+nosetests --with-coverage --cover-package=functional
+pylint functional
