bdaac9817a344d3f363ebf447a6c7308d5bd5a5f
Pedro Rodriguez <me@pedro.ai>
2020-07-12 14:01:59 -0400

    Fix generation of site

--- generate-site.sh
+++ generate-site.sh
@@ -1,6 +1,7 @@
 git checkout master -- README.md
-LAYOUT_PREFIX='---\nlayout: index\n---\n\n'
-echo $LAYOUT_PREFIX > index.md
+echo "---" > index.md
+echo "layout: index" >> index.md
+echo "---" >> index.md
 cat README.md >> index.md
 rm README.md
 
