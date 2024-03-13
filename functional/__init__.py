6ed2e9a8a73d97141a8a7edab25e1aefadc256a3
Pedro Rodriguez <me@pedro.ai>
2024-03-13 10:21:25 -0700

    Release 1.5.0

--- functional/__init__.py
+++ functional/__init__.py
@@ -5,9 +5,9 @@ the primary entrypoint at streams.seq
 from functional.streams import seq, pseq
 
 __author__ = "Pedro Rodriguez"
-__copyright__ = "Copyright 2021, Pedro Rodriguez"
+__copyright__ = "Copyright 2024, Pedro Rodriguez"
 __license__ = "MIT"
-__version__ = "1.4.3"
+__version__ = "1.5.0"
 __maintainer__ = "Pedro Rodriguez"
 __email__ = "me@pedro.ai"
 __status__ = "Production"
