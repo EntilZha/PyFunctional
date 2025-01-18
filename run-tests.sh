fd1b6381024c6701d1de7621f9f80bc15a71fde6
Samer Hamood <samer.hamood@gmail.com>
2025-01-18 15:31:58 +0000

    Make sure pipx is installed and print version if it is else exit script with error exit code

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,12 @@
 python_version=$(python --version | grep -Eo \[0-9\]\.\[0-9\]+\.\[0-9\]+)
 echo "Python version: $python_version"
 
+pipx_version=$(pipx --version)
+if [[ -z "$pipx_version" ]]; then
+  echo "Pipx is not installed"
+  exit 1
+else
+  echo "Pipx version:   $pipx_version"
+fi
+
 poetry run pytest
